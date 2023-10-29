8aadd17b65937ae85e3a4939a2a920df96cf44b2
sukhijab <sukhijab@student.ethz.ch>
2023-10-29 14:33:25 +0100

    added dependencies

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+required = [
+    'mbpo @ git+https://github.com/lasgroup/Model-based-policy-optimizers.git',
+    'bsm @ git+https://github.com/lasgroup/bayesian_statistical_models.git',
+]
+
+extras = {}
+setup(
+    name='mbrl',
+    version='0.0.1',
+    license="MIT",
+    packages=find_packages(),
+    python_requires='>=3.9',
+    install_requires=required,
+    extras_require=extras,
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
