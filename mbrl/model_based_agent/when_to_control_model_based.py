2e21531f030a5bf44ee22421c450cdeadb87f926
lenarttreven <lenart.treven44@gmail.com>
2024-04-25 13:30:15 +0200

    flow input now env_state, env_action and real time

--- mbrl/model_based_agent/when_to_control_model_based.py
+++ mbrl/model_based_agent/when_to_control_model_based.py
@@ -24,8 +24,18 @@ class ModelBasedAgentState:
 
 class WhenToControlModelBasedAgent(BaseModelBasedAgent):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self,
+                 dt: float,
+                 min_time_between_switches: float,
+                 max_time_between_switches: float,
+                 episode_time: float,
+                 *args,
+                 **kwargs):
         super().__init__(*args, **kwargs)
+        self.dt = dt
+        self.min_time_between_switches = min_time_between_switches
+        self.max_time_between_switches = max_time_between_switches
+        self.episode_time = episode_time
 
     def prepare_actor(self,
                       optimizer: BaseOptimizer,
@@ -42,22 +52,44 @@ class WhenToControlModelBasedAgent(BaseModelBasedAgent):
         actor.set_system(system=system)
         return actor
 
+    @staticmethod
+    def compute_time(pseudo_time: chex.Array,
+                     dt: float,
+                     t_min: float,
+                     t_max: float,
+                     env_time: chex.Array,
+                     time_horizon: float
+                     ) -> chex.Array:
+        time_for_action = ((t_max - t_min) / 2 * pseudo_time + (t_max + t_min) / 2)
+        return jnp.minimum((time_for_action // dt) * dt, time_horizon - env_time)
+
     def _collected_buffer_to_train_data(self,
                                         collected_buffer_state: ReplayBufferState):
         idx = jnp.arange(start=collected_buffer_state.sample_position, stop=collected_buffer_state.insert_position)
         all_data = jnp.take(collected_buffer_state.data, idx, axis=0, mode='wrap')
         all_transitions = self.collected_data_buffer._unflatten_fn(all_data)
-        # obs = [state, time_to_go]
+        # obs = [env_state, env_times]
         obs = all_transitions.observation
         # action = [env_action, time_to_control]
         actions = all_transitions.action
+
+        env_states, env_times = obs[..., :-1], obs[..., 1]
+        env_actions, pseudo_times_for_action = actions[..., :-1], actions[..., 1]
         rewards = all_transitions.reward.reshape(-1, 1)  # This should be only integrated reward
-        inputs = jnp.concatenate([obs, actions], axis=-1)
-        next_obs = all_transitions.next_observation[..., :-1]  # We remove time_to_go
+
+        times_for_action = self.compute_time(pseudo_times_for_action,
+                                             dt=self.dt,
+                                             t_min=self.min_time_between_switches,
+                                             t_max=self.max_time_between_switches,
+                                             env_time=env_times,
+                                             time_horizon=self.episode_time)
+
+        inputs = jnp.concatenate([env_states, env_actions, times_for_action[..., None]], axis=-1)
+        env_states_next = all_transitions.next_observation[..., :-1]  # We remove time_to_go
         if self.predict_difference:
-            target = next_obs - obs[..., :-1]
+            target = env_states_next - env_states
         else:
-            target = next_obs
+            target = env_states_next
         outputs = jnp.concatenate([target, rewards], axis=-1)  # append the integrated reward to the output
         return Data(inputs=inputs, outputs=outputs)
 
@@ -70,18 +102,26 @@ if __name__ == "__main__":
     from mbrl.utils.offline_data import PendulumOfflineData, WhenToControlWrapper
     from wtc.wrappers.ih_switching_cost import IHSwitchCostWrapper, ConstantSwitchCost
     from wtc.utils import discrete_to_continuous_discounting
+    from bsm.bayesian_regression import DeterministicEnsemble
 
-    log_wandb = False
+    log_wandb = True
     ENTITY = 'trevenl'
 
     base_env = PendulumEnv(reward_source='dm-control')
 
+    min_time_between_switches = 1 * base_env.dt
+    max_time_between_switches = 30 * base_env.dt
+    num_integrator_steps = 100
+    switch_cost = 0.1
+
     env = IHSwitchCostWrapper(base_env,
-                              num_integrator_steps=100,
-                              min_time_between_switches=1 * base_env.dt,
-                              max_time_between_switches=30 * base_env.dt,
-                              switch_cost=ConstantSwitchCost(value=jnp.array(1.0)),
+                              num_integrator_steps=num_integrator_steps,
+                              min_time_between_switches=min_time_between_switches,
+                              max_time_between_switches=max_time_between_switches,
+                              switch_cost=ConstantSwitchCost(value=jnp.array(0.0)),
                               time_as_part_of_state=True)
+
+    episode_time = base_env.dt * num_integrator_steps
 
 
     class TransitionReward(Reward):
@@ -94,7 +134,7 @@ if __name__ == "__main__":
                      reward_params: RewardParams,
                      x_next: chex.Array | None = None
                      ):
-            reward = jnp.array(-1.0)
+            reward = jnp.array(-switch_cost)
             reward_dist = Normal(reward, jnp.zeros_like(reward))
             return reward_dist, reward_params
 
@@ -102,7 +142,11 @@ if __name__ == "__main__":
             return {'dt': 0.05}
 
 
-    offline_data_gen = WhenToControlWrapper()
+    offline_data_gen = WhenToControlWrapper(
+        num_integrator_steps=num_integrator_steps,
+        min_time_between_switches=min_time_between_switches,
+        max_time_between_switches=max_time_between_switches
+    )
     key = jr.PRNGKey(0)
 
     offline_data = offline_data_gen.sample_transitions(key=key,
@@ -111,18 +155,22 @@ if __name__ == "__main__":
     offline_data = None
     horizon = 100
     model = BNNStatisticalModel(
-        input_dim=env.observation_size + env.action_size,
-        output_dim=env.observation_size + 1 - 1,  # One more for the reward -1 for env time
-        num_training_steps=1_000,
-        output_stds=1e-3 * jnp.ones(env.observation_size + 1 - 1),  # One more for the reward -1 for env_time
-        features=(64, 64, 64),
+        input_dim=env.observation_size + env.action_size - 1,  # -1 since we don't input env_time
+        output_dim=env.observation_size + 1 - 1,  # +1 for the reward -1 for env time
+        num_training_steps=30_000,
+        output_stds=1e-3 * jnp.ones(env.observation_size + 1 - 1),  # +1 for the reward -1 for env_time
+        beta=1.0 * jnp.ones(shape=(env.observation_size + 1 - 1,)),
+        features=(64,) * 3,
+        bnn_type=DeterministicEnsemble,
         num_particles=5,
         logging_wandb=log_wandb,
         return_best_model=True,
         eval_batch_size=64,
         train_share=0.8,
         eval_frequency=5_000,
+        weight_decay=0.0,
     )
+
     discount_factor = 0.99
     continuous_discounting = discrete_to_continuous_discounting(discrete_discounting=discount_factor,
                                                                 dt=env.dt)
@@ -193,6 +241,10 @@ if __name__ == "__main__":
         num_envs=1,
         num_eval_envs=1,
         log_to_wandb=log_wandb,
+        dt=env.dt,
+        min_time_between_switches=min_time_between_switches,
+        max_time_between_switches=max_time_between_switches,
+        episode_time=episode_time,
     )
 
     agent_state = agent.run_episodes(num_episodes=20,
