a8594c09c8d298c549292a3ad1a98a2ef8529d0c
sukhijab <sukhijab@student.ethz.ch>
2024-05-20 11:20:17 +0200

    wtc rccar experiment

--- experiments/wtc_rccar/launcher.py
+++ experiments/wtc_rccar/launcher.py
@@ -2,24 +2,24 @@ import exp
 from experiments.util import generate_run_commands, generate_base_command, dict_permutations
 
 general_configs = {
-    'project_name': ['WTC_RCCar_May12_14_00'],
+    'project_name': ['WTC_RCCar_May19_11_43'],
     'num_offline_samples': [0],
-    'sac_horizon': [64],
+    'sac_horizon': [100],
     'deterministic_policy_for_data_collection': [0],
     'seed': list(range(5)),
-    'num_episodes': [75],
+    'num_episodes': [100],
     'sac_steps': [500_000],
     'min_bnn_steps': [5_000],
     'max_bnn_steps': [50_000],
-    'linear_scheduler_steps': [10_000, 20_000],
-    'first_episode_for_policy_training': [2],
-    'exploration': ['optimistic', 'pets'],
+    'linear_scheduler_steps': [20_000],
+    'first_episode_for_policy_training': [0],
+    'exploration': ['optimistic', 'pets', 'mean'],
     'reset_statistical_model': [1],
     'regression_model': ['FSVGD'],
     'max_time_factor': [5],
     'beta_factor': [2.0],
     'horizon': [100],
-    'transition_cost': [0.1, 0.2, 0.5]
+    'transition_cost': [0.2]
 }
 
 
@@ -35,7 +35,7 @@ def main():
                           num_cpus=1,
                           num_gpus=1,
                           mode='euler',
-                          duration='3:59:00',
+                          duration='23:59:00',
                           prompt=True,
                           mem=16000)
 
