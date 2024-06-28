12c6b2f19e61b1ab180cfe628bb2e8ec266ddb21
Christopher Biel <christopher.biel@tum.de>
2024-06-28 16:48:05 +0200

    Define the type of the agents passed

--- mbrl/model_based_agent/base_agent_wrapper.py
+++ mbrl/model_based_agent/base_agent_wrapper.py
@@ -1,9 +1,11 @@
 from mbrl.model_based_agent.base_model_based_agent import BaseModelBasedAgent
 from mbpo.optimizers.base_optimizer import BaseOptimizer
 from mbrl.model_based_agent.optimizer_wrapper import Actor
+from typing import Type
 
 class BaseAgentWrapper(BaseModelBasedAgent):
-    def __init__(self, agent_type, **kwargs):
+    def __init__(self, agent_type: Type[BaseModelBasedAgent],
+                 **kwargs):
         # Get the args and kwargs from the agent
         self.agent = agent_type(**kwargs)
         super().__init__(**kwargs)
