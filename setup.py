77b802e13223702bc63ddb704049308687102859
Klemens Iten <klemens.iten@hotmail.ch>
2024-07-09 15:20:04 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,6 @@ from setuptools import setup, find_packages
 
 required = [
     'pandas',
-    'numpy>=1.25.2',
-    'brax==0.10.0',
-    'chex>=0.1.82',
-    'flax==0.7.2',
-    'jax==0.4.14',
-    'jaxlib==0.4.14',  # Note: User must manually install using provided pip command for CUDA/CUDNN support
-    'jaxopt==0.8',
-    'jaxtyping==0.2.21',
-    'jaxutils==0.0.8',
-    'ml-collections==0.1.0',
-    'numba',
-    'scipy==1.11.2',
-    'setuptools>=68.1.2',
-    'tensorflow>=2.13.0',
     'mbpo @ git+https://github.com/lasgroup/Model-based-policy-optimizers.git',
     'bsm @ git+https://github.com/lasgroup/bayesian_statistical_models.git',
     'wtc @ git+ssh://git@github.com/lenarttreven/when_to_control.git',
@@ -31,7 +17,7 @@ setup(
     version='0.0.1',
     license="MIT",
     packages=find_packages(),
-    python_requires='>=3.11',
+    python_requires='>=3.9',
     install_requires=required,
     extras_require=extras,
     classifiers=[
