06459d53e5b5fe03b09a5a3f6689c390f9a1e379
Klemens Iten <klemens.iten@hotmail.ch>
2025-01-22 16:48:50 +0100

    reward margin experiments for dmc-pendulum

--- experiments/ct_pendulum_general_icem/launcher.py
+++ experiments/ct_pendulum_general_icem/launcher.py
@@ -1,7 +1,7 @@
 import exp
 from experiments.util import generate_run_commands, generate_base_command, dict_permutations
 
-PROJECT_NAME = 'CT_PendulumGeneral_Jan20_18_45_COMBRL_ccost_increase_gym'
+PROJECT_NAME = 'CT_PendulumGeneral_Jan22_16_45_COMBRL_ccost_rewmsrgin_vary_dmc'
 ENTITY = 'kiten'
 
 general_configs = {
@@ -12,9 +12,9 @@ general_configs = {
     'num_offline_samples': [0],
     'num_online_samples': [200],
     'deterministic_policy_for_data_collection': [0],
-    'reward_source': ['gym'],
-    'control_cost': [5, 10, 25, 100],
-    'num_episodes': [10],
+    'reward_source': ['dm-control'],
+    'control_cost': [0,0.02,0.05],
+    'num_episodes': [15],
     'bnn_steps': [15_000],
     'first_episode_for_policy_training': [0],
     'exploration': ['mean','ocorl'],
@@ -29,7 +29,7 @@ general_configs = {
     'eval_env': ['swing-up'],
     'reward_bound': [0.1],
     'reward_value_at_margin': [0.1],
-    'reward_margin_factor': [10],
+    'reward_margin_factor': [2,5,10],
 }
 
 sac_configs = (
