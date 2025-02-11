8823635473b5b151da405ed25f4203feef8133f4
Klemens Iten <klemens.iten@hotmail.ch>
2025-02-11 11:43:35 +0100

    added MountainCar experiments

---
+++ experiments/ct_mountaincar_general/launcher.py
@@ -0,0 +1,70 @@
+import exp
+from experiments.util import generate_run_commands, generate_base_command, dict_permutations
+
+PROJECT_NAME = 'CT_Mountaincar_Feb11_11_45_FirstTest'
+ENTITY = 'kiten'
+
+general_configs = {
+    'seed': list(range(3)),
+    'project_name': [PROJECT_NAME],
+    'entity': [ENTITY],
+    'optimizer': ['icem','sac'],
+    'num_offline_samples': [0],
+    'num_online_samples': [200],
+    'deterministic_policy_for_data_collection': [0],
+    'reward_source': ['gym'],
+    'num_episodes': [15],
+    'bnn_steps': [15_000],
+    'first_episode_for_policy_training': [0],
+    'exploration': ['optimistic','pets','ocorl','mean'],
+    'reset_statistical_model': [0],
+    'regression_model': ['probabilistic_ensemble'], # ,'GP'],
+    'beta': [2.0],
+    'weight_decay': [0.0],
+    'int_rew_weight_init': [1.0],
+    'int_rew_weight_end': [0.0],
+    'rew_decrease_steps': [10],
+}
+
+sac_configs = (
+    {
+        **general_configs,
+        'optimizer': ['sac'],
+        'train_steps_sac': [100_000],
+    }
+    if 'sac' in general_configs['optimizer']
+    else None
+)
+
+icem_configs = (
+    {
+        **general_configs,
+        'optimizer': ['icem'],
+        'optimizer_horizon': [20],
+        'icem_num_steps': [10],
+        'icem_colored_noise_exponent': [1.0],
+    }
+    if 'icem' in general_configs['optimizer']
+    else None
+)
+
+
+def main():
+    command_list = []
+    flags_combinations = dict_permutations(sac_configs) + dict_permutations(icem_configs)
+    for flags in flags_combinations:
+        cmd = generate_base_command(exp, flags=flags)
+        command_list.append(cmd)
+
+    # submit jobs
+    generate_run_commands(command_list,
+                          num_cpus=1,
+                          num_gpus=1,
+                          mode='euler',
+                          duration='23:59:00',
+                          prompt=True,
+                          mem=16000)
+
+
+if __name__ == '__main__':
+    main()
